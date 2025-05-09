c9428a08ae307277845658fc786cf5dd8355edec
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-05-09 14:00:00 +0600

    Loading css and jquery file from all

--- functions.php
+++ functions.php
@@ -6,9 +6,23 @@
 // Theme Title
 add_theme_support('title-tag');
 
+//  Theme CSS and jQuery File calling
+function rifat_css_js_file_calling(){
+    wp_enqueue_style('rifat-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri() . './css/bootstrap.css', array(), 'v1.0', 'all');
+    wp_register_style('custom', get_template_directory_uri() . './css/custom.css', array(), 'v1.0', 'all');
+    wp_enqueue_style("bootstrap");
+    wp_enqueue_style("custom");
 
+    // jQuery
 
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap' ,get_template_directory_uri(  ).'/js/bootstrap.js',array(),'true');
+    wp_enqueue_script( 'main' ,get_template_directory_uri(  ).'/js/main.js',array(),'true');
+    
+}
 
+add_action('wp_enqueue_scripts', 'rifat_css_js_file_calling' );
 
 
 
